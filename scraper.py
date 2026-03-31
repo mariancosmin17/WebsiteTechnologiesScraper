c70cf22ed045a76d6b5d76e94c7d1cecc5358cb5
mariancosmin17 <161645867+mariancosmin17@users.noreply.github.com>
2026-03-31 20:54:53 +0300

    Test pe un site folosind beautfiulSoup pentru a testa extragerea unei tehnologii

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
 
-url_de_test="https://www.shopify.com"
+url_de_test="https://kyliecosmetics.com"
 
 headere_browser={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/120.0.0.0 Safari/537.36"
@@ -10,10 +11,26 @@ raspuns=requests.get(url_de_test,headers=headere_browser,timeout=10)
 
 print("cod primit: ",raspuns.status_code)
 
-print("\n Headerele http trimise de server:")
-for cheie,valoare in raspuns.headers.items():
-    print(f"{cheie}: {valoare}")
+tehnologii_gasite={}
 
-print("\n Primele 500 de carac din codul html:")
-html_extras=raspuns.text[0:500]
-print(html_extras)
+headere_lowercase={cheie.lower():valoare.lower() for cheie,valoare in raspuns.headers.items()}
+
+if 'server' in headere_lowercase and 'cloudfare' in headere_lowercase['server']:
+    tehnologii_gasite['Cloudfare']="cuvantul cloudfare gasit in headerul http"
+
+navigate=BeautifulSoup(raspuns.text,'html.parser')
+
+toate_scripturile=navigate.find_all('script')
+
+for script in toate_scripturile:
+    sursa_script=script.get('src')
+    if sursa_script and 'cdn.shopify.com' in sursa_script:
+        tehnologii_gasite['Shopify']=f"am gasit un fisier js care incarca de la {sursa_script}"
+        break
+
+if len(tehnologii_gasite) > 0:
+    for tehnologie,dovada in tehnologii_gasite.items():
+        print(f"{tehnologie}")
+        print(f" : {dovada}")
+else:
+    print("nu am detectat nimic")
