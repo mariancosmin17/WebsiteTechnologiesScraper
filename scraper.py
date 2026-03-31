fcd077b6746bb181ba6c13e6b331ec2256564254
mariancosmin17 <161645867+mariancosmin17@users.noreply.github.com>
2026-03-31 20:23:12 +0300

    Test folosind biblioteca requests

---
+++ scraper.py
@@ -0,0 +1,11 @@
+import requests
+
+url_de_test="https://www.shopify.com"
+
+headere_browser={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
+raspuns=requests.get(url_de_test,headers=headere_browser,timeout=10)
+
+print("cod primit: ",raspuns.status_code)
